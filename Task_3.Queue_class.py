5579152205c9297c1be3e54141e3c3c87a21e3bd
DianaSW535 <sttcwvs@gmail.com>
2025-01-28 20:27:39 +0500

    Add files via upload

---
+++ Task_3.Queue_class.py
@@ -0,0 +1,31 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            raise IndexError('Очередь пуста.')
+
+    def start_peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError('Стек пуст.')
+
+    def finish_peek(self):
+        if not self.is_empty():
+            return self.items[0]
+        else:
+            raise IndexError('Стек пуст.')
+
+    def size(self):
+        return len(self.items)
+
