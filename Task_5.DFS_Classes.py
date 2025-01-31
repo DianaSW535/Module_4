a0790f5ba1a5eaefea6f442a14b31b1234722a0e
DianaSW535 <sttcwvs@gmail.com>
2025-01-31 14:10:09 +0500

    Add files via upload

---
+++ Task_5.DFS_Classes.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.outbound = []
+        self.inbound = []
+
+    def point_to(self, other):
+        self.outbound.append(other)
+        other.inbound.append(self)
+
+    def __str__(self):
+        return f'Node({self.value})'
+
+class Graph:
+    def __init__(self, root):
+        self.root = root
+
+    def dfs(self, node, visited = None):
+        if visited is None:
+            visited = []
+        if node in visited:
+            return visited
+        visited.append(node)
+        for next_node in node.outbound:
+            self.dfs(next_node, visited)
+        return visited
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+a.point_to(b)
+b.point_to(c)
+c.point_to(d)
+d.point_to(a)
+b.point_to(d)
+
+g = Graph(a)
+
+visited_nodes = g.dfs(g.root)
+print(*[str(node) for node in visited_nodes])
